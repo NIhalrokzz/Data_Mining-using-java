b8e8eba45cb6088ab60b1f66e21378b8ea695b7d
NIhalrokzz <singhnihal838@gmail.com>
2019-06-14 15:25:31 +0530

    Add files via upload

---
+++ src/operations/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operations;
+
+/**
+ *
+ * @author pallavi
+ */
+public class Constants {
+    
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+    public static final String DBURL = "jdbc:mysql://localhost:3306/roadaccidentindia";
+}
